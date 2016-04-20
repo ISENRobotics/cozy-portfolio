30fcb8bdd47bfbbc277bc4c114263f5f198f303c
FlorentinDUBOIS <dubois.florentin@live.fr>
2016-04-20 19:55:23 +0200

    delete test log

--- server.js
+++ server.js
@@ -5,10 +5,7 @@
 // -----------------------------------------------------------------------------
 // requirements
 var americano = require( 'americano' );
-var logger    = require( './server/models/logger' );
 
-
-logger.info( 'zdsqkfsj' );
 // -----------------------------------------------------------------------------
 // launch americano server
 americano.start({
