3f0cb93edd0e396c7a212334dd76cb3f23d25b1c
FlorentinDUBOIS <dubois.florentin@live.fr>
2016-04-20 19:54:21 +0200

    append printit log

--- server.js
+++ server.js
@@ -5,7 +5,10 @@
 // -----------------------------------------------------------------------------
 // requirements
 var americano = require( 'americano' );
+var logger    = require( './server/models/logger' );
 
+
+logger.info( 'zdsqkfsj' );
 // -----------------------------------------------------------------------------
 // launch americano server
 americano.start({
--- server/models/logger.js
+++ server/models/logger.js
@@ -1,11 +1,44 @@
 // -----------------------------------------------------------------------------
 // requirements
 var winston = require( 'winston' );
+var printit = require( 'printit' );
+var log     = printit({
+    prefix: "portfolio",
+    date: true 
+});
+
+log.log = function( level, message ) {
+    switch( level ) {
+        case 'error': {
+            log.error( message );
+            
+            break;
+        }
+        
+        case 'debug': {
+            log.debug( message );
+            
+            break;
+        }
+        
+        case 'warn': {
+            log.warn( message );
+            
+            break;
+        }
+        
+        default: {
+            log.info( message );   
+        
+            break;
+        }
+    }  
+};
 
 // -----------------------------------------------------------------------------
 // exports
 module.exports = new winston.Logger({
     transports: [
-        new winston.transports.Console()
+        log
     ]
 });
