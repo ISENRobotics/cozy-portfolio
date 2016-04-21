9822a65b3d951d328b6865274682ea269b6cc931
FlorentinDUBOIS <dubois.florentin@live.fr>
2016-04-21 16:31:31 +0200

    Touch of god

--- server/config.js
+++ server/config.js
@@ -1,27 +1,61 @@
+// ----------------------------------------------------------------------------
+// requirements
 var americano   = require( 'americano' );
+var path        = require( 'path' );
 var uuid        = require( 'uuid' );
 var helmet      = require( 'helmet' );
 var bparser     = require( 'body-parser' );
 var cparser     = require( 'cookie-parser' );
 var compression = require( 'compression' );
 var morgan      = require( 'morgan' );
+var logger      = require( './models/logger' );
 
+// ----------------------------------------------------------------------------
+// utils
+var router      = americano.Router();
 var secret      = uuid.v4();
 
+// ----------------------------------------------------------------------------
+// common configs
+var extensions = [];
+
+// ----------------------------------------------------------------------------
+// ?? - I don't know what it does
+logger.info( 'Load americano methodOverride' );
+extensions.push( americano.methodOverride());
+
+// ----------------------------------------------------------------------------
+// active httpd configs
+logger.info( 'Load server configurations' );
+extensions.push( helmet());
+extensions.push( compression());
+extensions.push( bparser.urlencoded({ extended: true }));
+extensions.push( bparser.json());
+extensions.push( cparser( secret ));
+
+// ----------------------------------------------------------------------------
+// Bind static routes
+logger.info( 'Load static routes' );
+var routes = [
+    { syml: '/public/vendor/angular', real: 'node_modules/angular' },
+    { syml: '/public/vendor/angular', real: 'node_modules/angular-aria' },
+    { syml: '/public/vendor/angular', real: 'node_modules/angular-route' },
+    { syml: '/public/vendor/angular', real: 'node_modules/angular-translate/dist' },
+    { syml: '/public/stylesheets',    real: 'client/stylesheets' },
+    { syml: '/public/assets',         real: 'client/assets' },
+    { syml: '/',                      real: 'client/interface' }
+];
+
+for( var i in routes ) {
+    extensions.push( router.use( routes[i].syml, americano.static( path.join( __dirname, '..', routes[i].real ))));
+}
+
+// ----------------------------------------------------------------------------
+// create settings
+logger.info( 'Export configurations settings' );
 module.exports = {
     common: {
-        use: [
-            helmet(),
-            compression(),
-            bparser.urlencoded({ extended: true }),
-            bparser.json(),
-            cparser( secret ),
-            americano.methodOverride(),               
-            americano.static( __dirname + '/../client' ),
-            americano.static( __dirname + '/node_modules/angular' ),
-            americano.static( __dirname + '/node_modules/angular-aria' ),
-            americano.static( __dirname + '/node_modules/angular-route' )
-        ],
+        use: extensions,
         
         useAfter: [
             americano.errorHandler({
