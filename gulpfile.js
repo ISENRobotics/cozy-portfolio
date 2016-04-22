268b880bbeddb98c03ccc822e435463a0fd5f519
FlorentinDUBOIS <dubois.florentin@live.fr>
2016-04-22 15:17:23 +0200

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ gulp.task( 'default', ['watch']);
 // watch task
 gulp.task( 'watch', ['build'], function() {
     gulp.watch(['client/stylesheets/**/*.scss'], ['build:scss']);
-    gulp.watch(['client/interface/app/**/!(application.min).js'], ['build:js']);
+    gulp.watch(['client/interface/app/**/*.js'], ['build:js']);
 });
 
 // ----------------------------------------------------------------------------
@@ -21,16 +21,14 @@ gulp.task( 'build', ['build:scss', 'build:js']);
 // ----------------------------------------------------------------------------
 // build:js
 gulp.task( "build:js", function() {
-    return gulp.src(['client/interface/app/**/!(application|application.min|*.lang).js'])
+    return gulp.src(['client/interface/app/**/!(*.lang).js'])
                .pipe( plugins.plumberNotifier())
-               .pipe( plugins.concat( 'application.js' ))
-               .pipe( gulp.dest( 'client/interface/app' ))
+               .pipe( plugins.concat( 'application.min.js' ))
+               .pipe( gulp.dest( 'client/interface' ))
                .pipe( require( 'vinyl-named' )())
                .pipe( require( 'webpack-stream' )())
-               .pipe( gulp.dest( 'client/interface/app' ))
-               .pipe( plugins.rename({ suffix: ".min" }))
                .pipe( plugins.uglify({ compress: {}}))
-               .pipe( gulp.dest( 'client/interface/app' ));    
+               .pipe( gulp.dest( 'client/interface' ));    
 });
 
 // ----------------------------------------------------------------------------
@@ -40,8 +38,7 @@ gulp.task( 'build:scss', function() {
                .pipe( plugins.plumberNotifier())
                .pipe( plugins.sass.sync())
                .pipe( plugins.autoprefixer())
-               .pipe( gulp.dest( 'client/stylesheets/dist/' ))
                .pipe( plugins.cssnano())
                .pipe( plugins.rename({ suffix: '.min' }))
-               .pipe( gulp.dest( 'client/stylesheets/dist/' ));
+               .pipe( gulp.dest( 'client/stylesheets/dist' ));
 });
