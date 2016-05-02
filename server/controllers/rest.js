70573b410660a5d86c2c0c8d367cf7c29d68f52d
GillouBZH <github@gillesbiannic.fr>
2016-05-02 10:59:01 +0200

    API REST 90% completed

--- server/controllers/rest.js
+++ server/controllers/rest.js
@@ -1,34 +1,137 @@
 var logger = require( '../models/logger' );
-var portfolio = require('../models/portfolio');
+var pfolio = require('../models/portfolio');
+
+var getPortfolio = function( callback ) {
+    pfolio.request('all', function(err, portfolios) {
+        if(err) {
+            /* If error, log and return it */
+            logger.error( err.message );
+            callback( err );
+        } else if ( !portfolios[0] ) {
+            /* If no portfolio exists actually, init portfolio */
+            logger.info( "Cannot find any portfolio, trying to initialize one ..." );
+            resetPortfolio( function( err, portfolio ) {
+                if(err) {
+                    callback( err );
+                } else {
+                    callback( null, portfolio );
+                }
+            });
+        } else {
+            /* If portfolio exists, log and return it */
+            logger.info( "Successfully found portfolio" );
+            callback( null, portfolios[0] );
+        }
+    });
+};
+
+var resetPortfolio = function( callback ) {
+    pfolio.request('all', function(err, portfolios) {
+        if(err) {
+            /* If error, log and return it */
+            logger.error( err.message );
+            callback( err );
+        } else if ( !portfolios[0] ) {
+            /* If no portfolio, create it */
+            createThePortfolio( function( err, portfolio ) {
+                if(err) {
+                    callback( err );
+                } else {
+                    callback( null, portfolio );
+                }
+            });
+        } else {
+            /* Delete portfolio, then create a new */
+            logger.info( "Delete portfolio" );
+            if( portfolios[1] ) {
+                /* If a second portfolio exists, we got problems ... */
+                logger.error( "More than ONE portfolio ... not possible !!!");
+            }
+            /* destroy portfolio 0 and rebuild a new */
+            pfolio.destroy( portfolios[0].id, function( err ) {
+                if(err) {
+                    logger.error( err.message );
+                    callback( err );
+                } else {
+                    logger.info( "Portfolio deleted" );
+                    createThePortfolio( function( err, portfolio ) {
+                        if(err) {
+                            callback( err );
+                        } else {
+                            callback( null, portfolio );
+                        }
+                    });
+                }
+            });
+        }
+    });
+};
+
+var createThePortfolio = function( callback ) {
+    pfolio.create( {}, function(err, portfolio) {
+        if(err) {
+            /* If error, log and return it */
+            logger.error( err.message );
+            callback( err );
+        } else {
+            logger.info( "Portfolio created" );
+            callback( null, portfolio );
+        }
+    });
+};
+
+var updateResource = function( req, callback ) {
+    getPortfolio( function( err, portfolio ) {
+        if(err) {
+            /* If error, log and return it */
+            logger.error( err.message );
+            callback( err );
+        } else {
+            /* Create a json with : Resource:dataReceived . Update it */
+            var newData = {};
+            newData[ req.params.resource ] = req.body;
+            portfolio.updateAttributes( newData, function(err, dataUpdated) {
+                if(err) {
+                    logger.error( err.message );
+                    callback( err );
+                } else {
+                    logger.info( "Update attributes for : " + req.params.resource );
+                    callback( null, dataUpdated );
+                }
+            });
+        }
+    });
+};
 
 module.exports = {
     get: function( req, res ) {
-        portfolio.request('all', function(err, portfolios) {
+        /* get the portfolio and return the resource */
+        getPortfolio( function( err, portfolio ) {
             if(err) {
-                logger.error( err.message );
-            } else if ( !portfolios ) {
-                logger.debug( "Cannot find any portfolio, it must be undefined actually" );
-                // Creer par le par-defaut
+                res.sendStatus(500);
             } else {
-                logger.info( "Load " + req.params.resource );
-                res.json( portfolios[0][req.params.resource] );
+                res.json( portfolio[req.params.resource] );
             }
         });
     },
-    post: function( req, res ) {
-        // portfolio.create( {}, function(err, data) {
-        //     if(err) {
-        //         logger.error( err.message );
-        //     } else {
-        //         logger.debug( "Added data : " + data );
-                res.json( {} );
-        //     }
-        // });
-    },
     delete: function( req, res ) {
-        res.json( {} );
+        /* reset the portfolio */
+        resetPortfolio( function( err, portfolio ) {
+            if(err) {
+                res.sendStatus(500);
+            } else {
+                res.json( portfolio );
+            }
+        });
     },
     update: function( req, res ) {
-        res.json( {} );
+        /* replace the actual content of resource by the req.body */
+        updateResource( req, function( err, dataUpdated ) {
+            if(err) {
+                res.sendStatus(500);
+            } else {
+                res.json( dataUpdated );
+            }
+        });
     }
 };
