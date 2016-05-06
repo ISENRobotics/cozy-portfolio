9f627fd7e8b7fc2a90e43cea6140086e9e790628
GillouBZH <github@gillesbiannic.fr>
2016-05-06 14:23:25 +0200

    testing tests

--- server/server.test.js
+++ server/server.test.js
@@ -2,21 +2,12 @@ var http = require ('http');
 
 module.exports = function( QUnit ) {
     QUnit.test( 'routes test', function( assert ) {
-        assert.expect(2);
-        var done = assert.async(2);
+        assert.expect(1);
+        var done = assert.async(1);
 
         http.get( {
             port: process.env.PORT | 9200,
             path: '/portfolio'
-        },
-        function( res ) {
-            assert.equal( res.statusCode, 200 );
-            done();
-        });
-
-        http.get( {
-            port: process.env.PORT | 9200,
-            path: '/portfolio/banner'
         },
         function( res ) {
             assert.equal( res.statusCode, 200 );
