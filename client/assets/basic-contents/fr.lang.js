9e08e73422c7b081cbbfb9d33b8c6474af73b9c3
GillouBZH <github@gillesbiannic.fr>
2016-05-09 12:17:36 +0200

    save

--- client/assets/basic-contents/fr.lang.js
+++ client/assets/basic-contents/fr.lang.js
@@ -1,3 +1,39 @@
 {
-    
+    "banner" : {
+        "header" : "Portfolio",
+        "icon" : "",
+        "avatar" : "",
+        "content" : ""
+    },
+    "presentation" : {
+        "header" : "",
+        "content" : ""
+    },
+    "contact" : {
+        "header" : "Contactez-moi"
+    },
+    "educations" : {
+        "header" : "Ma formation",
+        "contents" : [
+
+        ]
+    },
+    "skills" : {
+        "header" : "Mes compétences",
+        "contents" : [
+
+        ]
+    },
+    "experiences" : {
+        "header" : "Mon expériences",
+        "contents" : [
+
+        ]
+    },
+    "hobbies" : {
+        "header" : "Mes hobbies et loisirs",
+        "contents" : [
+            
+        ]
+    }
 }
